17575dad8df53e94784d5e1c92df4fbb692cf422
unknown <tommyconner96@gmail.com>
2020-06-26 20:35:15 -0400

    db structured in knex migration

--- data/migrations/20200626195953_projects.js
+++ data/migrations/20200626195953_projects.js
@@ -1,8 +1,45 @@
 
-exports.up = async function(knex) {
-  
+exports.up = async function (knex) {
+    await knex.schema.createTable("projects", (tbl) => {
+        tbl.increments("id")
+        tbl.text("project_name")
+            .unique()
+            .notNullable()
+        tbl.text("description")
+        tbl.boolean("completed")
+            .notNullable()
+            .defaultTo(false)
+    })
+
+    await knex.schema.createTable("resources", (tbl) => {
+        tbl.increments()
+        tbl.text("resource_name")
+            .unique()
+            .notNullable()
+        tbl.text("description")
+    })
+
+    await knex.schema.createTable("tasks", (tbl) => {
+        tbl.increments()
+        tbl.integer("project_id")
+            .references("id")
+            .inTable("projects")
+            .unsigned()
+            .notNullable()
+            .onUpdate("CASCADE")
+            .onDelete("CASCADE")
+        tbl.text("description")
+            .notNullable()
+        tbl.text("notes")
+        tbl.boolean("completed")
+            .defaultTo(false)
+            .notNullable()
+    })
+
 }
 
-exports.down = async function(knex) {
-  
+exports.down = async function (knex) {
+    await knex.schema.dropTableIfExists("tasks")
+    await knex.schema.dropTableIfExists("resources")
+    await knex.schema.dropTableIfExists("projects")
 }
